a201113f407549d2ab7c479c25bbeb1ec13c83c3
admin <admin@adminhedeMacBook-Pro.local>
2016-10-19 18:00:21 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <search-bar></search-bar>
+  </div>
+</template>
+
+<script>
+import SearchBar from './components/search-bar'
+
+export default {
+  name: 'app',
+  components: {
+    SearchBar
+  }
+}
+</script>
+
+<style>
+  @import "assets/styles/search.css";
+  @import "assets/styles/reset.css";
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/search-bar.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <form id="form" action="" method="post">
+      <span class="s_ipt_wr quickdelete-wrap">
+        <input name="keyword" value="" class="s_ipt">
+      </span>
+      <span>
+        <input type="submit" class="btn" value="搜索">
+      </span>
+    </form>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+</script>
